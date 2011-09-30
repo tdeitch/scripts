e868bdb6221442368db6cd02669e70c6eb678ece
Trey Deitch <me@tdeitch.com>
2011-09-30 08:38:31 -0700

    Added more commands

--- hw.sh
+++ hw.sh
@@ -21,48 +21,38 @@ fi
 
 if [ "$2" == "physics" ]; then
     FOLDER=$HOME/Stanford/PHYSICS61
-    if [ "$3" == "pset" ]; then
-        SUFFIX="answers.tex"
-        TEMPLATE="template.tex"
-        if [ "$1" == "new" ]; then
-            DATE=`date +%m%d`
-            NUMBER=`ls $FOLDER | grep -c $SUFFIX`
-            NUMBER=`expr $NUMBER + 1`
-            cp $FOLDER/$TEMPLATE $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
-            bbedit $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
-        fi
-        if [ "$1" == "open" ]; then
-            bbedit $FOLDER/`ls $FOLDER | grep $SUFFIX | tail -n 1`
-        fi
-    fi
-    if [ "$3" == "book" ]; then
-        SUFFIX="text.pdf"
-        if [ "$1" == "open" ]; then
-            open $FOLDER/$SUFFIX
-        fi
-    fi
 fi
 
 if [ "$2" == "math" ]; then
     FOLDER=$HOME/Stanford/MATH51
-        if [ "$3" == "pset" ]; then
-        SUFFIX="answers.tex"
-        TEMPLATE="template.tex"
-        if [ "$1" == "new" ]; then
-            DATE=`date +%m%d`
-            NUMBER=`ls $FOLDER | grep -c $SUFFIX`
-            NUMBER=`expr $NUMBER + 1`
-            cp $FOLDER/$TEMPLATE $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
-            bbedit $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
-        fi
-        if [ "$1" == "open" ]; then
-            bbedit $FOLDER/`ls $FOLDER | grep $SUFFIX | tail -n 1`
-        fi
+fi
+
+if [ "$3" == "pset" ]; then
+    SUFFIX="answers.tex"
+    PDF="answers.pdf"
+    TEMPLATE="template.tex"
+    if [ "$1" == "new" ]; then
+        DATE=`date +%m%d`
+        NUMBER=`ls $FOLDER | grep -c $SUFFIX`
+        NUMBER=`expr $NUMBER + 1`
+        cp $FOLDER/$TEMPLATE $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
+        bbedit $FOLDER/$DATE-pset-$NUMBER-$SUFFIX
     fi
-    if [ "$3" == "book" ]; then
-        SUFFIX="text.pdf"
-        if [ "$1" == "open" ]; then
-            open $FOLDER/$SUFFIX
-        fi
+    if [ "$1" == "open" ]; then
+        bbedit $FOLDER/`ls $FOLDER | grep $SUFFIX | tail -n 1`
+    fi
+    if [ "$1" == "compile" ]; then
+        pushd $FOLDER
+        pdflatex `ls $FOLDER | grep $SUFFIX | tail -n 1`
+        trash *.aux
+        trash *.log
+        open `ls $FOLDER | grep $PDF | tail -n 1`
+    fi
+fi
+
+if [ "$3" == "book" ]; then
+    SUFFIX="text.pdf"
+    if [ "$1" == "open" ]; then
+        open $FOLDER/$SUFFIX
     fi
 fi
