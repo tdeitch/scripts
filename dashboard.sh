76d8e8c26d64f2d5affc00503d6dccdf087b085c
Trey Deitch <me@tdeitch.com>
2011-09-30 08:36:10 -0700

    Created script to display various information

---
+++ dashboard.sh
@@ -0,0 +1,22 @@
+offlineimap -u Noninteractive.Quiet
+echo "
+
+Fastmail
+--------
+`/opt/heirloom/5bin/mailx -H -f /Users/trey/Mail/Fastmail/INBOX/`
+
+
+Stanford
+--------
+`/opt/heirloom/5bin/mailx -H -f /Users/trey/Mail/Stanford/INBOX/`
+
+
+Tasks
+-----
+`tasks`
+
+
+Calendar
+--------
+`icalBuddy -nc eventsToday+4`
+"
